56b987bbd25995f145afdc51c22728e8cb86762a
guozejiong <guozejiong@sailvan.com>
2024-02-23 13:53:31 +0800

    还原

--- embedchain/vectordb/elasticsearch.py
+++ embedchain/vectordb/elasticsearch.py
@@ -298,62 +298,11 @@ class ElasticsearchDB(BaseVectorDB):
         or_conditions: dict[str, any],
         size: int = 5
     ) -> Union[list[tuple[str, dict]], list[str]]:
-        _source = ["text", "metadata"]
-
-        query = {
-            "min_score": 1,
-            "query": {"bool": {"should": [{"match": {"text": input_query}}]}}
-        }
-        if and_conditions is not None:
-            for field, value in and_conditions.items():
-                if isinstance(value, list):
-                    query["query"]["bool"].setdefault("must", []).append({"terms": {field: value}})
-                else:
-                    query["query"]["bool"].setdefault("must", []).append({"match": {field: value}})
-
-        if or_conditions is not None:
-            for field, value in or_conditions.items():
-                if isinstance(value, list):
-                    query["query"]["bool"].setdefault("should", []).append({"terms": {field: value}})
-                else:
-                    query["query"]["bool"].setdefault("should", []).append({"match": {field: value}})
-        response = self.client.search(index=self._get_index(), body=query, _source=_source, size=20)
-        docs = response["hits"]["hits"]
-        ids = []
-        for doc in docs:
-            ids.append(doc["_id"])
-
-        input_query_vector = self.embedder.embedding_fn(input_query)
-        query_vector = input_query_vector[0]
-
-        knn_query = {
-            "min_score": 1,
-            "query": {"ids":{"values":ids}},
-            "knn": {
-            "field": "embeddings",
-            "query_vector": query_vector,
-            "k": 5,
-            "num_candidates": 20
-            }
-        }  
-
-        response = self.client.search(index=self._get_index(), body=knn_query, _source=_source, size=size)
-
-
-        # input_query_vector = self.embedder.embedding_fn(input_query)
-        # query_vector = input_query_vector[0]     
-
         # _source = ["text", "metadata"]
 
         # query = {
         #     "min_score": 1,
-        #     "query": {"bool": {"should": [{"match": {"text": input_query}}]}},
-        #     "knn": {
-        #     "field": "embeddings",
-        #     "query_vector": query_vector,
-        #     "k": 5,
-        #     "num_candidates": 20
-        #     }
+        #     "query": {"bool": {"should": [{"match": {"text": input_query}}]}}
         # }
         # if and_conditions is not None:
         #     for field, value in and_conditions.items():
@@ -368,7 +317,58 @@ class ElasticsearchDB(BaseVectorDB):
         #             query["query"]["bool"].setdefault("should", []).append({"terms": {field: value}})
         #         else:
         #             query["query"]["bool"].setdefault("should", []).append({"match": {field: value}})
-        # response = self.client.search(index=self._get_index(), body=query, _source=_source, size=size)
+        # response = self.client.search(index=self._get_index(), body=query, _source=_source, size=20)
+        # docs = response["hits"]["hits"]
+        # ids = []
+        # for doc in docs:
+        #     ids.append(doc["_id"])
+
+        # input_query_vector = self.embedder.embedding_fn(input_query)
+        # query_vector = input_query_vector[0]
+
+        # knn_query = {
+        #     "min_score": 1,
+        #     "query": {"ids":{"values":ids}},
+        #     "knn": {
+        #     "field": "embeddings",
+        #     "query_vector": query_vector,
+        #     "k": 5,
+        #     "num_candidates": 20
+        #     }
+        # }  
+
+        # response = self.client.search(index=self._get_index(), body=knn_query, _source=_source, size=size)
+
+
+        input_query_vector = self.embedder.embedding_fn(input_query)
+        query_vector = input_query_vector[0]     
+
+        _source = ["text", "metadata"]
+
+        query = {
+            "min_score": 1,
+            "query": {"bool": {"should": [{"match": {"text": input_query}}]}},
+            "knn": {
+            "field": "embeddings",
+            "query_vector": query_vector,
+            "k": 5,
+            "num_candidates": 20
+            }
+        }
+        if and_conditions is not None:
+            for field, value in and_conditions.items():
+                if isinstance(value, list):
+                    query["query"]["bool"].setdefault("must", []).append({"terms": {field: value}})
+                else:
+                    query["query"]["bool"].setdefault("must", []).append({"match": {field: value}})
+
+        if or_conditions is not None:
+            for field, value in or_conditions.items():
+                if isinstance(value, list):
+                    query["query"]["bool"].setdefault("should", []).append({"terms": {field: value}})
+                else:
+                    query["query"]["bool"].setdefault("should", []).append({"match": {field: value}})
+        response = self.client.search(index=self._get_index(), body=query, _source=_source, size=size)
         
         # query = {
         #     "script_score": {
