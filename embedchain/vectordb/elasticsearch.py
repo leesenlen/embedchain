22ee89a11040bdd540240ef0ff9f4da63e00e758
guozejiong <guozejiong@sailvan.com>
2024-04-08 13:51:44 +0800

    claude model token计数

--- embedchain/vectordb/elasticsearch.py
+++ embedchain/vectordb/elasticsearch.py
@@ -450,6 +450,8 @@ class ElasticsearchDB(BaseVectorDB):
         elif model == "gpt-3.5-turbo-0301":
             tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
             tokens_per_name = -1  # if there's a name, the role is omitted
+        elif model == "claude-3-sonnet":
+            return self.num_tokens_from_string(messages,"cl100k_base")
         else:
             raise NotImplementedError(
                 f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
